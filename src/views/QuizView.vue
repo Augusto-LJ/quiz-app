b79bb62d4c3666fb5b121d7a0e7c605b5d671691
Augusto-LJ <augustolimajardim@gmail.com>
2025-09-15 14:51:17 -0300

    feat: dinamically fills the header

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -2,20 +2,25 @@
     import Question from "@/components/Question.vue"
     import QuizHeader from "@/components/QuizHeader.vue"
     import {useRoute} from "vue-router"
-    import {ref} from "vue"
+    import {ref, computed} from "vue"
     import quizes from "@/data/quizes.json"
 
     const route = useRoute();
     const quizId = parseInt(route.params.id);
     const quiz = quizes.find(x => x.id === quizId);
     const currentQuestionIndex = ref(0);
+    const questionStatus = computed(() => `${currentQuestionIndex.value}/${quiz.questions.length}`);
+    const barPercentage = computed(() => `${currentQuestionIndex.value/quiz.questions.length * 100}%`)
 </script>
 
 <template>
     <div>
-        <QuizHeader/>
+        <QuizHeader
+            :questionStatus="questionStatus"
+            :barPercentage="barPercentage"/>
         <div>
             <Question :question="quiz.questions[currentQuestionIndex]"/>
         </div>
+        <button @click="currentQuestionIndex++">Next question</button>
     </div>
 </template>
